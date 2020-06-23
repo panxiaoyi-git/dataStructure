d91d5383caf7b1d0452f98cfeb9ad5b5f7a3960e
panxiaoyi <993276623@qq.com>
2020-06-23 16:01:43 +0800

    HashMap第二次提交测试

--- src/hashmap/HashMapTest.java
+++ src/hashmap/HashMapTest.java
@@ -12,5 +12,6 @@ public class HashMapTest {
         System.out.println(zhangsan.toString());
         System.out.println("杨小洲是煞笔");
         System.out.println(zhangsan.toString());
+        System.out.println("杨小洲是煞笔");
     }
 }
