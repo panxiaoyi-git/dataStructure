9ec3ebd3289c29fd5c56307cc9be3ea0fb78cb00
panxiaoyi <993276623@qq.com>
2020-07-08 22:13:45 +0800

    6666

--- src/hashmap/HashMapTest.java
+++ src/hashmap/HashMapTest.java
@@ -15,6 +15,7 @@ public class HashMapTest {
         System.out.println("杨小洲是煞笔");
         System.out.println(1111);
         System.out.println(2222);
+        System.out.println("whereis");
 
     }
 }
